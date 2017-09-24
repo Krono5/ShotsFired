9f9c331f37cff0e9a912d59d3ff4b99c8d2aa087
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-09-24 00:18:54 -0400

    change header

--- index.php
+++ index.php
@@ -47,9 +47,10 @@
 		<div class="container">
 			<div class="row ">
 				<div class="col-md-8 col-sm-6">
-					<div class="head-main">ShotsFired</div>
-					<div class="head-sub-main">The Stream Team</div>
+					<h1 class="head-main">ShotsFired</h1>
+					<h2 class="head-sub-main">The Stream Team</h2>
 					<div class="head-last">
+
 						Doing dumb shit since '13.
 					</div>
 
