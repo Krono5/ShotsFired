2453bfbfc5a76205aebf10b8b356e52c27f34015
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-09-23 23:49:27 -0400

    fix icons

--- index.php
+++ index.php
@@ -92,91 +92,86 @@
 	<!--End Header section  -->
 	<!--Services Section-->
 	<section id="services">
-		<div class="container">
-
-			<div class="row text-center">
-				<div class="col-md-8 col-md-offset-2">
-					<h2>What We Do</h2>
-				</div>
+		<div class="row text-center">
+			<div class="col-md-8 col-md-offset-2">
+				<h2>What We Do</h2>
 			</div>
+		</div>
 
 
-			<div class="row text-center space-pad">
-				<div class="col-md-4 col-sm-4">
-					<div>
-						<i class="fa fa-bullhorn" style="font-size: 400%"></i>
-						<h3>Blogging</h3>
-						<p>
-							Gaming, Tech, and More! Join the discussion above.
-						</p>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-4">
-					<div>
-						<i class="fa fa-twitch" style="font-size: 400%"></i>
-						<h3>Streaming</h3>
-						<p>
-							Singleplayer, Co-op, and multiplayer! OH MY! Links above.
-						</p>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-4">
-					<div>
-						<i class="fa fa-youtube-play" style="font-size: 400%"></i>
-						<h3>Youtube</h3>
-						<p>
-							I ain't a clip show, but I'll su... make you laugh.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="row ">
-				<div class="col-md-6 col-sm-6">
-					<h3>Lorem ipsum dolor sit amet</h3>
+		<div class="row text-center space-pad">
+			<div class="col-md-4 col-sm-4">
+				<div>
+					<i class="fa fa-bullhorn" style="font-size: 400%"></i>
+					<h3>Blogging</h3>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec nisl odio. Mauris vehicula at nunc id posuere. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec nisl odio. Mauris vehicula at nunc id posuere. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
+						Gaming, Tech, and More! Join the discussion above.
 					</p>
 				</div>
-				<div class="col-md-6 col-sm-6">
-					<div class="panel panel-default">
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div>
+					<i class="fa fa-twitch" style="font-size: 400%"></i>
+					<h3>Streaming</h3>
+					<p>
+						Singleplayer, Co-op, and multiplayer! OH MY! Links above.
+					</p>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div>
+					<i class="fa fa-youtube-play" style="font-size: 400%"></i>
+					<h3>Youtube</h3>
+					<p>
+						I ain't a clip show, but I'll su... make you laugh.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="row ">
+			<div class="col-md-6 col-sm-6">
+				<h3>Lorem ipsum dolor sit amet</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec nisl odio. Mauris vehicula at nunc id posuere. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec nisl odio. Mauris vehicula at nunc id posuere. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 
-						<div class="panel-body">
-							<ul class="nav nav-pills" style="background-color: #ECECEC;">
-								<li class="active"><a href="#home-pills" data-toggle="tab">Home</a>
-								</li>
-								<li class=""><a href="#profile-pills" data-toggle="tab">Profile</a>
-								</li>
-								<li class=""><a href="#messages-pills" data-toggle="tab">Messages</a>
-								</li>
-								<li class=""><a href="#settings-pills" data-toggle="tab">Settings</a>
-								</li>
-							</ul>
+				</p>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<div class="panel panel-default">
 
-							<div class="tab-content">
-								<div class="tab-pane fade  active in" id="home-pills">
-									<h4>Home Tab</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, officia deserunt mollit anim id est laborum.</p>
-								</div>
-								<div class="tab-pane fade" id="profile-pills">
-									<h4>Profile Tab</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, officia deserunt mollit anim id est laborum.</p>
+					<div class="panel-body">
+						<ul class="nav nav-pills" style="background-color: #ECECEC;">
+							<li class="active"><a href="#home-pills" data-toggle="tab">Home</a>
+							</li>
+							<li class=""><a href="#profile-pills" data-toggle="tab">Profile</a>
+							</li>
+							<li class=""><a href="#messages-pills" data-toggle="tab">Messages</a>
+							</li>
+							<li class=""><a href="#settings-pills" data-toggle="tab">Settings</a>
+							</li>
+						</ul>
 
-								</div>
-								<div class="tab-pane fade" id="messages-pills">
-									<h4>Messages Tab</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, officia deserunt mollit anim id est laborum.</p>
-								</div>
-								<div class="tab-pane fade" id="settings-pills">
-									<h4>Settings Tab</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, officia deserunt mollit anim id est laborum.</p>
-								</div>
+						<div class="tab-content">
+							<div class="tab-pane fade  active in" id="home-pills">
+								<h4>Home Tab</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, officia deserunt mollit anim id est laborum.</p>
+							</div>
+							<div class="tab-pane fade" id="profile-pills">
+								<h4>Profile Tab</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, officia deserunt mollit anim id est laborum.</p>
+
+							</div>
+							<div class="tab-pane fade" id="messages-pills">
+								<h4>Messages Tab</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, officia deserunt mollit anim id est laborum.</p>
+							</div>
+							<div class="tab-pane fade" id="settings-pills">
+								<h4>Settings Tab</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, officia deserunt mollit anim id est laborum.</p>
 							</div>
 						</div>
 					</div>
-
 				</div>
-
 			</div>
 		</div>
 
