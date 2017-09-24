7ae63a87ec45262a423709c6efb6eb35d0d896b2
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-09-23 21:32:44 -0400

    change tyler description

--- about.php
+++ about.php
@@ -63,8 +63,10 @@
           <div class="col-sm-3 col-xs-6">
             <div class="card card-block">
               <a href="#"><img alt="" class="team-img" src="img/Kr0n05.jpg">
-              <h4 class="card-title">KR0N05</h4>
-              <p class="card-text">Odega's Babysitter</p>
+              <div class="card-title-wrap">
+                <span class="card-title">KR0N05</span>
+                <span class="card-text">Odega's Babysitter</span>
+              </div>
 
               <div class="team-over">
                 <h4 class="hidden-md-down">
