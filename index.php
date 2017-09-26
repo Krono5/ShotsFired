a782c77ca4c94c5a79183699936b336e159d4c92
rmwiesenberg <rmwiesenberg@wpi.edu>
2017-09-25 22:27:49 -0400

    fix head

--- index.php
+++ index.php
@@ -65,11 +65,7 @@
 														"Odega");
 							$q_idx=rand(0,2);
 						?>
-						<h3 align="left" style="padding-left:15px;
-																		padding-top:200px;
-																		padding-bottom:0px;
-																		font-size: 20;
-																		color: #fff">
+						<h3 align="left" class="rand-quote">
 							<i>"<?php echo($quote[$q_idx]) ?>"</i>
 						</h3>
 						<p align="right" style="padding-right:20px; padding-bottom:0px; color: #fff">
