0912680bf42784d00e66dbbb6e0b5261bbfbb0a1
rmwiesenberg <ryan.wiesenberg@toyota.com>
2018-01-23 17:53:33 -0500

    adding mc 3d

--- minecraft.php
+++ minecraft.php
@@ -39,7 +39,7 @@
 	</div>
 
 	<iframe
-	  frameborder="0" style="border:0; width: 100%"
+	  frameborder="0" style="border:0; width: 100%; height:100%"
 	  src="assets/overviewer/ftb_revelation_1.12/index.html" allowfullscreen>
 	</iframe>
 
