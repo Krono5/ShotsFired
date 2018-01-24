989643846537d401b176fd72f0efacae0c579c64
Ryan <rwiesenberg603@gmail.com>
2018-01-23 21:37:44 -0500

    try to include mapping

--- minecraft.php
+++ minecraft.php
@@ -39,8 +39,7 @@
 	</div>
 
 	<iframe
-	  style="border:100; position: absolute;
-    	top: 0; bottom: 0; left: 0; right: 0;  height: (100%-border); width: 100%;"
+	  style="position: absolute; top: 100px; bottom: 0; left: 0; right: 0;  height: (100%-100px); width: 100%;"
 	  src="ftb_revelation_1.12/api.html" allowfullscreen>
 	</iframe>
 
