df090322909728597ebafeb72197a0dc2294d20e
Ryan <rwiesenberg603@gmail.com>
2018-01-24 00:04:28 -0500

    moving to general resource folder

--- index.php
+++ index.php
@@ -141,10 +141,7 @@
 
 
 		</section>
-		<!--footer Section -->
-		<div id="footer">
-			<?php include("footer.php");?>
-		</div>
+		<?php include("footer.php");?>
 
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
