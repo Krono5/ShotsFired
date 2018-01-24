9548de2ee0688b27f082da96b2cc6d4e67186158
Ryan <rwiesenberg603@gmail.com>
2018-01-24 00:00:33 -0500

    moving to general resource folder

---
+++ assets/resources.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>v
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>ShotsFired Team</title>
+	<link rel="icon" type="image/png" sizes="96x96" href="icon/favicon-96x96.png">
+	
+	<!--REQUIRED STYLE SHEETS-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="assets/css/style.css" />
+</head>
---
+++ header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<?php include('assets/resources.php'); ?>
+	</head>
+	<body>
+		 <header>
+			<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				<div class="container">
+					<a href="/" class="navbar-brand"><img src="assets/img/logo_white.png" class="img-responsive"> </a>
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<!-- Collect the nav links for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li><div class="dropdown text-center">
+		 						<button class="dropbtn" onclick="window.location='/'">HOME</button>
+							</div></li>
+
+							<li><div class="dropdown text-center">
+		 						<button class="dropbtn">MEDIA</button>
+		  							<div class="dropdown-content">
+		  								<a href="/blog">
+		  										Blog</a>	    							
+		    							<a href="/streams.php">
+												Streams</a>
+										<a href="https://www.youtube.com/channel/UCuhs4HbeDc6XEqaxg3Y_L1Q" target="_blank">
+												YouTube</a>
+		  							</div>
+							</div></li>
+
+							<li><div class="dropdown text-center">
+		 						<button class="dropbtn">JOIN US</button>
+		  							<div class="dropdown-content">
+		    							<a href="/discord.php">Discord</a>
+		    							<a href="/minecraft.php">Minecraft</a>
+		  							</div>
+							</div></li>
+
+							<li><div class="dropdown text-center">
+		 						<button class="dropbtn" onclick="window.location='/about.php'">ABOUT</button>
+							</div></li>
+
+
+
+							<li><div class="dropdown text-center">
+		 						<button class="dropbtn" onclick="window.location='/contact.php'">CONTACT</button>
+							</div></li>
+
+						</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+				</div>
+				<!-- /.container -->
+			</nav>
+		</header>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
+</html>
