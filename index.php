1094987ea8710d29bcaaad3940eae254b336ef58
Ryan <rwiesenberg603@gmail.com>
2018-01-23 23:30:12 -0500

    fix css

--- index.php
+++ index.php
@@ -17,8 +17,7 @@
 	<title>ShotsFired Team</title>
 	<link rel="icon" type="image/png" sizes="96x96" href="icon/favicon-96x96.png">
 	<!--REQUIRED STYLE SHEETS-->
-	<link rel="stylesheet" href="bootstrap.min.css">
-	<link rel="stylesheet" href="bootstrap-table.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 	<!-- GOOGLE FONT -->
@@ -178,13 +177,9 @@
 		<?php include("footer.html");?>
 	</div>
 
-	<script src="jquery.min.js"></script>
-	<script src="bootstrap.min.js"></script>
-	<script src="bootstrap-table.js"></script>
-	<script src="bootstrap-table-zh-CN.js"></script>
-	<script src="assets/plugins/jquery-1.10.2.js"></script>
-	<script src="assets/plugins/4jquery.parallax-1.1.3.js"></script>
-	<script src="assets/js/custom.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
