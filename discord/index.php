29df51db4ddac519c541f49a13e50e1c63380e67
rmwiesenberg <ryan.wiesenberg@toyota.com>
2018-01-25 11:02:06 -0500

    test discord shift

---
+++ discord/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <?php include('assets/resources.php'); ?>
+	</head>
+
+	<body>
+		<?php include('header.php'); ?>
+		
+
+		<iframe src="https://discordapp.com/widget?id=130742910265065472&theme=dark" width="480" height="720" allowtransparency="true" frameborder="0"></iframe>
+
+
+
+		<?php include("footer.php");?>
+	</body>
+</html>
