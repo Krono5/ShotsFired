f2211a6c5c11f0af543fa688ba700e647111528d
Ryan <rwiesenberg603@gmail.com>
2018-01-25 00:11:16 -0500

    fix discord.php sizing

--- discord.php
+++ discord.php
@@ -8,10 +8,7 @@
 		<?php include('header.php'); ?>
 		
 
-		<iframe 
-			style="height: 100%; width: 100%;"
-			src="https://discordapp.com/widget?id=130742910265065472&theme=dark">			
-		</iframe>
+		<iframe src="https://discordapp.com/widget?id=130742910265065472&theme=dark" width="100%" height="100%" allowtransparency="true" frameborder="0"></iframe>
 
 
 
