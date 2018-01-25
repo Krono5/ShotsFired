02cab7a11e06e99a26d5baa4604c8308fd8e723d
Ryan <rwiesenberg603@gmail.com>
2018-01-24 22:08:40 -0500

    header navbar fix

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 		<?php include('assets/resources.php'); ?>
 	</head>
 	<body>
-	 	<nav class="navbar navbar-inverse">
+	 	<nav class="navbar navbar-inverse" id="header">
 			<div class="container-fluid">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#headerNavbar">
