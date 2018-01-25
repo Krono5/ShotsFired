5e1cb09c7e1c50b89f63e96b5ffae7af2dfedd4c
Ryan <rwiesenberg603@gmail.com>
2018-01-24 20:59:26 -0500

    header navbar fix

--- header.php
+++ header.php
@@ -5,57 +5,55 @@
 		<?php include('assets/resources.php'); ?>
 	</head>
 	<body>
-		 <header>
-		 	<nav class="navbar navbar-inverse" role="navigation">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#headerNavbar">
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span> 
-				        </button>
-						<a href="/" class="navbar-brand"><img class="navbar-logo" src="assets/img/logo_white.png"> </a>
-					</div>
-					<div class="collapse navbar-collapse" id="headerNavbar">
-						<u1 class="nav navbar-nav navbar-right">
-							<li><div class="dropdown text-center">
-		 						<button class="dropbtn" onclick="window.location='/'">HOME</button>
-							</div></li>
-
-							<li><div class="dropdown text-center">
-		 						<button class="dropbtn">MEDIA</button>
-		  							<div class="dropdown-content">
-		  								<a href="/blog">
-		  										Blog</a>	    							
-		    							<a href="/streams.php">
-												Streams</a>
-										<a href="https://www.youtube.com/channel/UCuhs4HbeDc6XEqaxg3Y_L1Q" target="_blank">
-												YouTube</a>
-		  							</div>
-							</div></li>
-
-							<li><div class="dropdown text-center">
-		 						<button class="dropbtn">JOIN US</button>
-		  							<div class="dropdown-content">
-		    							<a href="/discord.php">Discord</a>
-		    							<a href="/minecraft.php">Minecraft</a>
-		  							</div>
-							</div></li>
-
-							<li><div class="dropdown text-center">
-		 						<button class="dropbtn" onclick="window.location='/about.php'">ABOUT</button>
-							</div></li>
-
-
-
-							<li><div class="dropdown text-center">
-		 						<button class="dropbtn" onclick="window.location='/contact.php'">CONTACT</button>
-							</div></li>
-						</u1>
-					</div>
+	 	<nav class="navbar navbar-inverse" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#headerNavbar">
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span> 
+			        </button>
+					<a href="/" class="navbar-brand"><img class="navbar-logo" src="assets/img/logo_white.png"> </a>
 				</div>
-			</nav>
-		</header>
+				<div class="collapse navbar-collapse" id="headerNavbar">
+					<u1 class="nav navbar-nav navbar-right">
+						<li><div class="dropdown text-center">
+	 						<button class="dropbtn" onclick="window.location='/'">HOME</button>
+						</div></li>
+
+						<li><div class="dropdown text-center">
+	 						<button class="dropbtn">MEDIA</button>
+	  							<div class="dropdown-content">
+	  								<a href="/blog">
+	  										Blog</a>	    							
+	    							<a href="/streams.php">
+											Streams</a>
+									<a href="https://www.youtube.com/channel/UCuhs4HbeDc6XEqaxg3Y_L1Q" target="_blank">
+											YouTube</a>
+	  							</div>
+						</div></li>
+
+						<li><div class="dropdown text-center">
+	 						<button class="dropbtn">JOIN US</button>
+	  							<div class="dropdown-content">
+	    							<a href="/discord.php">Discord</a>
+	    							<a href="/minecraft.php">Minecraft</a>
+	  							</div>
+						</div></li>
+
+						<li><div class="dropdown text-center">
+	 						<button class="dropbtn" onclick="window.location='/about.php'">ABOUT</button>
+						</div></li>
+
+
+
+						<li><div class="dropdown text-center">
+	 						<button class="dropbtn" onclick="window.location='/contact.php'">CONTACT</button>
+						</div></li>
+					</u1>
+				</div>
+			</div>
+		</nav>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
