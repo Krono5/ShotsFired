78ba49a4002ca32158c28c5e61c77ed2dc3b0a8d
Ryan <rwiesenberg603@gmail.com>
2018-01-24 20:35:13 -0500

    header navbar fix

--- header.php
+++ header.php
@@ -6,47 +6,44 @@
 	</head>
 	<body>
 		 <header>
-			<nav class="navbar navbar-inverse" role="navigation">
-				<div class="container">
-					<a href="/" class="navbar-brand"><img class="navbar-logo" src="assets/img/logo_white.png"> </a>
-					<nav class="navbar navbar-inverse navbar-right">
-						<li><div class="dropdown text-center">
-	 						<button class="dropbtn" onclick="window.location='/'">HOME</button>
-						</div></li>
+			<div class="container">
+				<a href="/" class="navbar-brand"><img class="navbar-logo" src="assets/img/logo_white.png"> </a>
+				<nav class="navbar navbar-inverse navbar-right" role="navigation">
+					<li><div class="dropdown text-center">
+ 						<button class="dropbtn" onclick="window.location='/'">HOME</button>
+					</div></li>
 
-						<li><div class="dropdown text-center">
-	 						<button class="dropbtn">MEDIA</button>
-	  							<div class="dropdown-content">
-	  								<a href="/blog">
-	  										Blog</a>	    							
-	    							<a href="/streams.php">
-											Streams</a>
-									<a href="https://www.youtube.com/channel/UCuhs4HbeDc6XEqaxg3Y_L1Q" target="_blank">
-											YouTube</a>
-	  							</div>
-						</div></li>
+					<li><div class="dropdown text-center">
+ 						<button class="dropbtn">MEDIA</button>
+  							<div class="dropdown-content">
+  								<a href="/blog">
+  										Blog</a>	    							
+    							<a href="/streams.php">
+										Streams</a>
+								<a href="https://www.youtube.com/channel/UCuhs4HbeDc6XEqaxg3Y_L1Q" target="_blank">
+										YouTube</a>
+  							</div>
+					</div></li>
 
-						<li><div class="dropdown text-center">
-	 						<button class="dropbtn">JOIN US</button>
-	  							<div class="dropdown-content">
-	    							<a href="/discord.php">Discord</a>
-	    							<a href="/minecraft.php">Minecraft</a>
-	  							</div>
-						</div></li>
+					<li><div class="dropdown text-center">
+ 						<button class="dropbtn">JOIN US</button>
+  							<div class="dropdown-content">
+    							<a href="/discord.php">Discord</a>
+    							<a href="/minecraft.php">Minecraft</a>
+  							</div>
+					</div></li>
 
-						<li><div class="dropdown text-center">
-	 						<button class="dropbtn" onclick="window.location='/about.php'">ABOUT</button>
-						</div></li>
-
+					<li><div class="dropdown text-center">
+ 						<button class="dropbtn" onclick="window.location='/about.php'">ABOUT</button>
+					</div></li>
 
 
-						<li><div class="dropdown text-center">
-	 						<button class="dropbtn" onclick="window.location='/contact.php'">CONTACT</button>
-						</div></li>
 
-					</nav>
-				</div>
-			</nav>
+					<li><div class="dropdown text-center">
+ 						<button class="dropbtn" onclick="window.location='/contact.php'">CONTACT</button>
+					</div></li>
+				</nav>
+			</div>
 		</header>
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
