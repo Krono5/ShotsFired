6866fa66997347d09266efb75e29ef1af9929962
Ryan <rwiesenberg603@gmail.com>
2018-01-24 23:39:10 -0500

    fix includes

--- footer.php
+++ footer.php
@@ -10,5 +10,8 @@
 				shotsfired.xyz </a>
 			| All Right Reserved
 		</div>
+		<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</body>
 </html>
