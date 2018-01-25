03ec9bad7fc12571fcdb397b0110fd72f3fed0f8
Ryan <rwiesenberg603@gmail.com>
2018-01-24 21:28:54 -0500

    header navbar fix

--- header.php
+++ header.php
@@ -52,8 +52,8 @@
 				</div>
 			</div>
 		</nav>
-		<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	</body>
 </html>
