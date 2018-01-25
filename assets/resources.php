5afa7526b459ca12d65f22e7d894260fdf1f3442
Ryan <rwiesenberg603@gmail.com>
2018-01-24 21:50:35 -0500

    header navbar fix

--- assets/resources.php
+++ assets/resources.php
@@ -9,6 +9,6 @@
 	<!--REQUIRED STYLE SHEETS-->
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">	
 	<link rel="stylesheet" href="assets/css/style.css" />
 </head>
