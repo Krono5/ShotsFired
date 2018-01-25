e44b89bcac46845d739a994851b0ca6c626d8f88
Ryan <rwiesenberg603@gmail.com>
2018-01-24 21:14:15 -0500

    header navbar fix

--- assets/resources.php
+++ assets/resources.php
@@ -7,9 +7,8 @@
 	<link rel="icon" type="image/png" sizes="96x96" href="icon/favicon-96x96.png">
 	
 	<!--REQUIRED STYLE SHEETS-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
-	<link rel="stylesheet" href="assets/css/font-awesome.min.css"/>
 	<link rel="stylesheet" href="assets/css/style.css" />
 </head>
