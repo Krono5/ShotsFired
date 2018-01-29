00fa013e7a761136dbb1e49efb9ce3aa1906c2d6
rmwiesenberg <ryan.wiesenberg@toyota.com>
2018-01-29 13:39:22 -0500

    footer adjust

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 					shotsfired.xyz </a>
 				| All Right Reserved
 			</div>
-		</footer>footer>
+		</footer>
 		<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
