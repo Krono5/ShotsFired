8ed35174c4800ba7d30a2cf0a18a5897eae838a2
rmwiesenberg <ryan.wiesenberg@toyota.com>
2018-01-29 13:23:40 -0500

    footer adjust

--- footer.php
+++ footer.php
@@ -4,14 +4,17 @@
     	<?php include('assets/resources.php'); ?>
   	</head>
 	<body>
-		<div class="for-full-back" id="footer">
-			&copy 2017
-			<a href="/#top_home">
-				shotsfired.xyz </a>
-			| All Right Reserved
-		</div>
+		<footer>
+			<div class="for-full-back" id="footer">
+				&copy 2017
+				<a href="/#top_home">
+					shotsfired.xyz </a>
+				| All Right Reserved
+			</div>
+		</footer>footer>
 		<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 	</body>
 </html>
