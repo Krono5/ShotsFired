f4a7c35771efd37b2c9249aa0183f111d0ed9a0d
rmwiesenberg <ryan.wiesenberg@toyota.com>
2018-01-29 15:36:55 -0500

    update general iframe

--- minecraft.php
+++ minecraft.php
@@ -9,16 +9,10 @@
 
 		<iframe
 		  	style="width:100%; height:calc(100% - 100px); 
-		  		 border:none; margin:0; padding:0; 
-		  		 overflow:hidden; display:block;"
+		  		 border:none; margin:0; padding:0; overflow:hidden;"
 		  	src="ftb_revelation_1.12/api.html" allowfullscreen>
 		</iframe>
-
-		<iframe 
-			style="margin:0; padding:0, z-index:99"
-			width="250" height="260" scrolling="no"  frameBorder="0" 
-			src="https://www.mine-land.it/status.php?ip=revelation.shotsfired.xyz"></iframe>
-
+		
 		<?php include('footer.php'); ?>
 	</body>
 </html>
