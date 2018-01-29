d4f6bdd7359f90e061cc6ab52157c02a9ff07c9f
rmwiesenberg <ryan.wiesenberg@toyota.com>
2018-01-29 14:15:07 -0500

    header adjust

--- minecraft.php
+++ minecraft.php
@@ -8,11 +8,16 @@
 		<?php include('header.php'); ?>
 
 		<iframe
-		  style="width:100%; height:calc(100% - 100px); 
+		  	style="width:100%; height:calc(100% - 100px); 
 		  		 border:none; margin:0; padding:0; 
 		  		 overflow:hidden; display:block;"
-		  src="ftb_revelation_1.12/api.html" allowfullscreen>
+		  	src="ftb_revelation_1.12/api.html" allowfullscreen>
 		</iframe>
+
+		<iframe 
+			style="margin:0; padding:0, z-index:99"
+			width="250" height="260" scrolling="no"  frameBorder="0" 
+			src="https://www.mine-land.it/status.php?ip=revelation.shotsfired.xyz"></iframe>
 
 		<?php include('footer.php'); ?>
 	</body>
