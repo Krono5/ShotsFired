246602c3acf1881cab576cec2db8b38339c6a634
rmwiesenberg <ryan.wiesenberg@toyota.com>
2018-01-29 13:32:57 -0500

    minecraft adjust

--- minecraft.php
+++ minecraft.php
@@ -8,7 +8,7 @@
 		<?php include('header.php'); ?>
 
 		<iframe
-		  style="width:100%; height:calc(100% - 50px); 
+		  style="width:100%; height:calc(100% - 100px); 
 		  		 border:none; margin:0; padding:0; 
 		  		 overflow:hidden; z-index:999999;"
 		  src="ftb_revelation_1.12/api.html" allowfullscreen>
