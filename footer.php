c5f2bddb17eb7868606281e586a9a0e9092ad09c
rmwiesenberg <ryan.wiesenberg@toyota.com>
2018-01-29 15:37:54 -0500

    remove loading assets from header and footer

--- footer.php
+++ footer.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-    	<?php include('assets/resources.php'); ?>
-  	</head>
 	<body>
 		<footer>
 			<div class="for-full-back" id="footer">
@@ -14,6 +11,5 @@
 		<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
 	</body>
 </html>
