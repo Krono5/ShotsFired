dc1581bbb134bdd6bdfa6da88451db2d2ffbbd95
ShotsFired <shotsfiredteam@gmail.com>
2018-01-30 19:52:58 -0500

    update dynbmap

--- minecraft.php
+++ minecraft.php
@@ -10,7 +10,7 @@
 		<iframe
 		  	style="width:100%; height:calc(100% - 100px); 
 		  		 border:none; margin:0; padding:0; overflow:hidden;"
-		  	src="http://localhost:24825" allowfullscreen>
+		  	src="http://shotsfired.xyz:24825" allowfullscreen>
 		</iframe>
 		
 		<?php include('footer.php'); ?>
